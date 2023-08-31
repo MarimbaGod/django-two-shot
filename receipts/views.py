686f2e261bb3c26d1719b96fd1def2497bd80640
Kyle <bossertkt@gmail.com>
2023-08-31 14:30:01 -0700

    feature 6

--- receipts/views.py
+++ receipts/views.py
@@ -1,15 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from receipts.models import Receipt
 
 
 # Create your views here.
 # unsure about how well I did at defining this view function...
 def receipts_list(request):
-    receipts = Receipt.objects.all
+    receipts = Receipt.objects.all()
     context = {
         "receipts": receipts,
     }
     return render(request, "receipts/list.html", context)
-
-
-# im not sure why I named things reciepts, or why I use c
