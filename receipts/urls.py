47b3c511c382ede6721cc2e35131dba8f5a591f7
Kyle <bossertkt@gmail.com>
2023-09-03 18:25:07 -0700

    feature 12 complete

--- receipts/urls.py
+++ receipts/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
-from receipts.views import receipts_list, create_receipt, redirect
+from receipts.views import (
+    receipts_list,
+    create_receipt,
+    redirect,
+    expense_category_list,
+    accounts_list,
+)
 
 
 def redirect_to_receipts(request):
@@ -9,4 +15,6 @@ def redirect_to_receipts(request):
 urlpatterns = [
     path("", receipts_list, name="home"),
     path("create/", create_receipt, name="create_receipt"),
+    path("categories/", expense_category_list, name="category_list"),
+    path("accounts/", accounts_list, name="accounts_list"),
 ]
--- receipts/views.py
+++ receipts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from receipts.models import Receipt
+from receipts.models import Receipt, ExpenseCategory, Account
 from django.contrib.auth.decorators import login_required
 from receipts.forms import CreateReceiptForm
 
@@ -29,3 +29,31 @@ def create_receipt(request):
         "form": form,
     }
     return render(request, "receipts/create.html", context)
+
+
+@login_required
+def expense_category_list(request):
+    categories = ExpenseCategory.objects.filter(owner=request.user)
+    for category in categories:
+        category.receipts_count = Receipt.objects.filter(
+            category=category
+        ).count()
+
+        context = {
+            "categories": categories,
+        }
+        return render(request, "receipts/categories.html", context)
+
+
+@login_required
+def accounts_list(request):
+    accounts = Account.objects.filter(owner=request.user)
+    for account in accounts:
+        account.receipts_count = Receipt.objects.filter(
+            account=account
+        ).count()
+
+        context = {
+            "accounts": accounts,
+        }
+        return render(request, "receipts/accounts_list.html", context)
